7e419b72ace277892622fa56ad5acbf368a7b95c
D0d0 <jozef.d13@gmail.com>
2015-10-16 17:29:32 +0200

    Add inheritance

--- js/helpers.js
+++ js/helpers.js
@@ -1,3 +1,21 @@
+Function.prototype.inheritsFrom = function( parentClassOrObject ){
+    if ( parentClassOrObject.constructor == Function )
+    {
+        //Normal Inheritance
+        this.prototype = new parentClassOrObject;
+        this.prototype.constructor = this;
+        this.prototype.parent = parentClassOrObject.prototype;
+    }
+    else
+    {
+        //Pure Virtual Inheritance
+        this.prototype = parentClassOrObject;
+        this.prototype.constructor = this;
+        this.prototype.parent = parentClassOrObject;
+    }
+    return this;
+};
+
 if (!window.requestAnimationFrame) {
 
     window.requestAnimationFrame = (function () {

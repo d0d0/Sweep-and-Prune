f55d69adb8e2fd35cbbfe6a5d8b69dca7ca5d0c2
D0d0 <jozef.d13@gmail.com>
2016-01-05 16:30:35 +0100

    Implement MatrixPairManagement

--- js/MatrixPairManagement.js
+++ js/MatrixPairManagement.js
@@ -1,26 +1,45 @@
 "use strict";
 
-function MatrixPairManagement() {
-    this.pair = [];
+function MatrixPairManagement(size) {
+    this._matrix = [];
+
+    for (let i = 0; i < size; i++) {
+        this._matrix.push([]);
+        for (let j = 0; j < size; j++) {
+            this._matrix[this._matrix.length - 1].push(false);
+        }
+    }
 }
 
 MatrixPairManagement.prototype = {
     constructor: MatrixPairManagement,
     addPair: function (id1, id2) {
 
-        this.pair.push({
-            id1, id2
-        });
+        this._matrix[id1][id2] = true;
     },
     removePair: function (id1, id2) {
 
-
+        this._matrix[id1][id2] = false;
     },
     findPair(id1, id2){
 
+        return this._matrix[id1][id2];
     },
     getPairs: function () {
+        let result = [];
 
+        for (let i = 0; i < this._matrix.length; i++) {
+            for (let j = 0; j < this._matrix[i].length; j++) {
+                if (this._matrix[i][j]) {
+                    result.push({
+                        a: i,
+                        b: j
+                    });
+                }
+            }
+        }
+
+        return result;
     },
     toString: function () {
 

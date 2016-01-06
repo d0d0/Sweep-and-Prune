b12846abe833edf7951c8112a695227bfe4801eb
D0d0 <jozef.d13@gmail.com>
2016-01-06 19:18:26 +0100

    Improve AABB

--- js/AABB.js
+++ js/AABB.js
@@ -11,11 +11,11 @@ AABB.prototype = {
 
         for (var i = 0; i < rectangles.length; i++) {
             for (var j = i + 1; j < rectangles.length; j++) {
-                var overLapX = this._valueOverlap(rectangles[i].getX(), rectangles[j].getX(), rectangles[j].getX() + rectangles[j].getWidth()) ||
-                        this._valueOverlap(rectangles[j].getX(), rectangles[i].getX(), rectangles[i].getX() + rectangles[i].getWidth()),
-                    overLapY = this._valueOverlap(rectangles[i].getY(), rectangles[j].getY(), rectangles[j].getY() + rectangles[j].getHeight()) ||
-                        this._valueOverlap(rectangles[j].getY(), rectangles[i].getY(), rectangles[i].getY() + rectangles[i].getHeight());
-                if (overLapX && overLapY) {
+                var overLap = (this._valueOverlap(rectangles[i].getX(), rectangles[j].getX(), rectangles[j].getX() + rectangles[j].getWidth()) ||
+                    this._valueOverlap(rectangles[j].getX(), rectangles[i].getX(), rectangles[i].getX() + rectangles[i].getWidth())) &&
+                    (this._valueOverlap(rectangles[i].getY(), rectangles[j].getY(), rectangles[j].getY() + rectangles[j].getHeight()) ||
+                    this._valueOverlap(rectangles[j].getY(), rectangles[i].getY(), rectangles[i].getY() + rectangles[i].getHeight()));
+                if (overLap) {
                     pairs.push({
                         a: i,
                         b: j
